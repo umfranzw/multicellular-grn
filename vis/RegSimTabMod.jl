69bbfc234811f3d424ccf13420d0e455437fcbe6
Wayne Franz <umfranzw@cs.umanitoba.ca>
2019-12-09 17:42:49 -0600

    Work on vis

--- vis/RegSimTabMod.jl
+++ vis/RegSimTabMod.jl
@@ -25,126 +25,27 @@ mutable struct Controls
     cell::Control
 end
 
-mutable struct Graphs
-    genome_graph::GtkImage
-    tree_graph::GtkImage
-end
-
 function build(
     run::Run,
     ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
     reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}}
 )
-    vbox = GtkBox(:v)
-    control_area, controls, graphs = build_control_area(run, ea_pops, reg_trees)
-    props_area = build_props_area(run, ea_pops, reg_trees)
-    
-    paned = GtkPaned(:h)
-    genome_pane = build_genome_pane(run, ea_pops, reg_trees, controls, graphs)
-    tree_pane = build_tree_pane(run, ea_pops, reg_trees, controls, graphs)
-    push!(paned, genome_pane)
-    push!(paned, tree_pane)
+    outer_vbox = GtkBox(:v)
 
-    hbox = GtkBox(:h)
-    push!(hbox, control_area)
-    push!(hbox, props_area)
-    
-    push!(vbox, paned)
-    push!(vbox, hbox)
-
-    vbox
-end
-
-function build_tree_pane(
-    run::Run,
-    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
-    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}},
-    controls::Controls,
-    graphs::Graphs
-)
-    pane = GtkBox(:v)
-    push!(pane, GtkLabel("Tree"))
-
-    build_tree_plot(run, ea_pops, reg_trees, controls, graphs)
-    push!(pane, graphs.tree_graph)
-    show(graphs.tree_graph)
-
-    pane
-end
-
-function build_genome_pane(
-    run::Run,
-    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
-    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}},
-    controls::Controls,
-    graphs::Graphs
-)
-    pane = GtkBox(:v)
-    push!(pane, GtkLabel("Genome"))
-    
-    build_genome_plot(run, ea_pops, reg_trees, controls, graphs)
-    push!(pane, graphs.genome_graph)
-    show(graphs.genome_graph)
-    
-    pane
-end
-
-function build_tree_plot(
-    run::Run,
-    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
-    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}},
-    controls::Controls,
-    graphs::Graphs
-)
-    indiv_index, ea_step, reg_step, cell_index = map(sym -> get_control_val(getfield(controls, sym)), (:indiv, :ea_step, :reg_step, :cell))
-    tree = reg_trees["after_reg_step"][ea_step][indiv_index][reg_step]
-
-    TreeVisMod.gen_graph(tree, tree_graph_path, cell_index)
-    set_gtk_property!(graphs.tree_graph, :file, tree_graph_path)
-end
-
-function build_genome_plot(
-    run::Run,
-    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
-    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}},
-    controls::Controls,
-    graphs::Graphs
-)
-    #reg_trees: ea_step, indiv, reg_step
-    indiv_index, ea_step, reg_step, cell_index = map(sym -> get_control_val(getfield(controls, sym)), (:indiv, :ea_step, :reg_step, :cell))
-    tree = reg_trees["after_reg_step"][ea_step][indiv_index][reg_step]
-    cell = CellTreeMod.get_bf_node(tree, cell_index)
-    proteins = ProteinStoreMod.get_all(cell.proteins)
-
-    concs = zeros(run.num_genes, length(proteins))
-    labels = fill("", (1, length(proteins)))
-    for i in 1:length(proteins)
-        buf = IOBuffer()
-        ProteinPropsMod.show(buf, proteins[i].props)
-        seek(buf, 0)
-        label = chomp(read(buf, String)) #protein sequence string (remove the newline)
-
-        labels[:, i] = [label]
-        concs[:, i]= proteins[i].concs
-    end
-
-    plot = groupedbar(concs, bar_position=:stack, bar_width=0.5, label=labels);
-    savefig(plot, genome_graph_path);
-    set_gtk_property!(graphs.genome_graph, :file, genome_graph_path)
-end
-
-function setup_controls(
-    run::Run,
-    controls::Controls,
-    graphs::Graphs,
-    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
-    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}}
-)
-    vbox = GtkBox(:v)
+    #control area
+    controls = Controls(
+        Control("Individual: ", GtkEntry(), 1:1:run.pop_size), #indiv
+        Control("EA Step: ", GtkEntry(), run.step_range), #ea
+        Control("Reg Step: ", GtkEntry(), 1:1:run.reg_steps), #reg
+        Control("Cell: ", GtkEntry(), 1:1:1) #cell
+    )
+    controls_vbox = GtkBox(:v)
     
     control_syms = Array{Symbol, 1}([:indiv, :ea_step, :reg_step, :cell])
+    add_callbacks = Dict{Symbol, Function}()
     for i in 1:length(control_syms)
-        control = getfield(controls, control_syms[i])
+        sym = control_syms[i]
+        control = getfield(controls, sym)
         grid = GtkGrid()
         
         label = GtkLabel(control.title)
@@ -161,78 +62,24 @@ function setup_controls(
 
         up_callback = button -> adjust_control_val(control, 1, map(sym -> getfield(controls, sym), control_syms[i + 1 : end]))
         down_callback = button -> adjust_control_val(control, -1, map(sym -> getfield(controls, sym), control_syms[i + 1 : end]))
-        update_genome_graph_callback = button -> build_genome_plot(run, ea_pops, reg_trees, controls, graphs)
-        update_tree_graph_callback = button -> build_tree_plot(run, ea_pops, reg_trees, controls, graphs)
         update_cell_range_callback = button -> update_cell_range(controls, reg_trees)
-
+        
         signal_connect(up_callback, up_button, :clicked)
-        signal_connect(update_genome_graph_callback, up_button, :clicked)
-        signal_connect(update_tree_graph_callback, up_button, :clicked)
         signal_connect(update_cell_range_callback, up_button, :clicked)
         
         signal_connect(down_callback, down_button, :clicked)
-        signal_connect(update_genome_graph_callback, down_button, :clicked)
-        signal_connect(update_tree_graph_callback, down_button, :clicked)
         signal_connect(update_cell_range_callback, down_button, :clicked)
 
-        push!(vbox, grid)
+        add_callbacks[sym] = f -> map(button -> signal_connect(f, button, :clicked), (up_button, down_button))
+        
+        push!(controls_vbox, grid)
     end
     update_cell_range(controls, reg_trees)
 
-    vbox
-end
+    #props area
+    props_vbox = GtkBox(:v)
 
-function update_cell_range(controls::Controls, reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}})
-    indiv_index, ea_step, reg_step, cell_index = map(sym -> get_control_val(getfield(controls, sym)), (:indiv, :ea_step, :reg_step, :cell))
-    tree = reg_trees["after_reg_step"][ea_step][indiv_index][reg_step]
-    size = CellTreeMod.size(tree)
-    controls.cell.range = StepRange(controls.cell.range.start, controls.cell.range.step, size) # note: must construct a new range, since they're immutable
-end
-
-function adjust_control_val(control::Control, dir::Int64, reset::Array{Control, 1})
-    cur = parse(Int64, get_gtk_property(control.entry, :text, String))
-    cur = clamp(cur + dir * control.range.step, control.range.start, control.range.stop)
-    set_gtk_property!(control.entry, :text, string(cur))
-
-    for control in reset
-        reset_control_val(control)
-    end
-end
-
-function reset_control_val(control::Control)
-    set_gtk_property!(control.entry, :text, "1")
-end
-
-function get_control_val(control::Control)
-    parse(Int64, get_gtk_property(control.entry, :text, String))
-end
-
-function build_control_area(
-    run::Run,
-    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
-    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}}
-)
-    controls = Controls(
-        Control("Individual: ", GtkEntry(), 1:1:run.pop_size), #indiv
-        Control("EA Step: ", GtkEntry(), run.step_range), #ea
-        Control("Reg Step: ", GtkEntry(), 1:1:run.reg_steps), #reg
-        Control("Cell: ", GtkEntry(), 1:1:1) #cell
-    )
-    graphs = Graphs(GtkImage(), GtkImage())
-    vbox = setup_controls(run, controls, graphs, ea_pops, reg_trees)
-
-    vbox, controls, graphs
-end
-
-function build_props_area(
-    run::Run,
-    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
-    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}}
-)
-    vbox = GtkBox(:v)
-
-    #row: props, concs
-    conc_types = repeat([Float64], run.num_genes)
+    conc_types = repeat([Float64], run.num_genes) #props, concs
     store = GtkListStore(String, conc_types...)
 
     indiv_index, ea_step, reg_step, cell_index = map(sym -> get_control_val(getfield(controls, sym)), (:indiv, :ea_step, :reg_step, :cell))
@@ -259,10 +106,116 @@ function build_props_area(
         push!(view, concs_col)
     end
 
-    push!(vbox, GtkLabel("Proteins"))
-    push!(vbox, view)
+    push!(props_vbox, GtkLabel("Proteins"))
+    push!(props_vbox, view)
+
+    #tree graph
+    tree_vbox = GtkBox(:v)
+    push!(tree_vbox, GtkLabel("Tree"))
+    tree_graph = GtkImage()
+    build_tree_plot(run, ea_pops, reg_trees, controls, tree_graph)
+    push!(tree_vbox, tree_graph)
+    show(tree_graph)
+
+    for (sym, connector) in add_callbacks
+        connector(button -> build_tree_plot(run, ea_pops, reg_trees, controls, tree_graph))
+    end
+
+    #genome_graph
+    genome_vbox = GtkBox(:v)
+    push!(genome_vbox, GtkLabel("Genome"))
+    genome_graph = GtkImage()
+    build_genome_plot(run, ea_pops, reg_trees, controls, genome_graph)
+    push!(genome_vbox, genome_graph)
+    show(genome_graph)
+
+    for (sym, connector) in add_callbacks
+        connector(button -> build_genome_plot(run, ea_pops, reg_trees, controls, genome_graph))
+    end
+
+    #paned container for graphs
+    paned = GtkPaned(:h)
+    push!(paned, genome_vbox)
+    push!(paned, tree_vbox)
+
+    #hbox for control and props areas
+    hbox = GtkBox(:h)
+    push!(hbox, controls_vbox)
+    push!(hbox, props_vbox)
     
-    vbox
+    push!(outer_vbox, paned)
+    push!(outer_vbox, hbox)
+
+    outer_vbox
+end
+
+function build_tree_plot(
+    run::Run,
+    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
+    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}},
+    controls::Controls,
+    graph::GtkImage
+)
+    indiv_index, ea_step, reg_step, cell_index = map(sym -> get_control_val(getfield(controls, sym)), (:indiv, :ea_step, :reg_step, :cell))
+    tree = reg_trees["after_reg_step"][ea_step][indiv_index][reg_step]
+
+    TreeVisMod.gen_graph(tree, tree_graph_path, cell_index)
+    set_gtk_property!(graph, :file, tree_graph_path)
+end
+
+function build_genome_plot(
+    run::Run,
+    ea_pops::Dict{String, Array{Array{Individual, 1}, 1}},
+    reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}},
+    controls::Controls,
+    graph::GtkImage
+)
+    #reg_trees: ea_step, indiv, reg_step
+    indiv_index, ea_step, reg_step, cell_index = map(sym -> get_control_val(getfield(controls, sym)), (:indiv, :ea_step, :reg_step, :cell))
+    tree = reg_trees["after_reg_step"][ea_step][indiv_index][reg_step]
+    cell = CellTreeMod.get_bf_node(tree, cell_index)
+    proteins = ProteinStoreMod.get_all(cell.proteins)
+
+    concs = zeros(run.num_genes, length(proteins))
+    labels = fill("", (1, length(proteins)))
+    for i in 1:length(proteins)
+        buf = IOBuffer()
+        ProteinPropsMod.show(buf, proteins[i].props)
+        seek(buf, 0)
+        label = chomp(read(buf, String)) #protein sequence string (remove the newline)
+
+        labels[:, i] = [label]
+        concs[:, i]= proteins[i].concs
+    end
+
+    plot = groupedbar(concs, bar_position=:stack, bar_width=0.5, label=labels);
+    savefig(plot, genome_graph_path);
+    set_gtk_property!(graph, :file, genome_graph_path)
+end
+
+function update_cell_range(controls::Controls, reg_trees::Dict{String, Array{Array{Array{CellTree, 1}, 1}, 1}})
+    indiv_index, ea_step, reg_step, cell_index = map(sym -> get_control_val(getfield(controls, sym)), (:indiv, :ea_step, :reg_step, :cell))
+    tree = reg_trees["after_reg_step"][ea_step][indiv_index][reg_step]
+    size = CellTreeMod.size(tree)
+    controls.cell.range = StepRange(controls.cell.range.start, controls.cell.range.step, size) # note: must construct a new range, since they're immutable
+end
+
+function adjust_control_val(control::Control, dir::Int64, reset::Array{Control, 1})
+    cur = parse(Int64, get_gtk_property(control.entry, :text, String))
+    cur = clamp(cur + dir * control.range.step, control.range.start, control.range.stop)
+    set_gtk_property!(control.entry, :text, string(cur))
+
+    for control in reset
+        reset_control_val(control)
+    end
+end
+
+function reset_control_val(control::Control)
+    set_gtk_property!(control.entry, :text, "1")
+end
+
+function get_control_val(control::Control)
+    parse(Int64, get_gtk_property(control.entry, :text, String))
 end
 
 end
