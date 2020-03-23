f5edce502bb3774e95473237f481235006ef040c
umfranzw <umfranzw@cs.umanitoba.ca>
2020-03-23 17:15:05 -0500

    Making final changes

--- DiffusionMod.jl
+++ DiffusionMod.jl
@@ -16,32 +16,32 @@ struct TreeInfo
         cell_to_level = Dict{Cell, Int64}()
         level_to_cell = Dict{Int64, Array{Cell, 1}}()
         if tree.root != nothing
-            build_info(tree.root, cell_to_level, level_to_cell, 0)
+            build_info(tree.root, cell_to_level, level_to_cell, 1)
         end
 
-        TreeInfo(cell_to_level, level_to_cell)
+        new(cell_to_level, level_to_cell)
     end
 end
 
 function build_info(cell::Cell, cell_to_level::Dict{Cell, Int64}, level_to_cell::Dict{Int64, Array{Cell, 1}}, level::Int64)
     cell_to_level[cell] = level
     if level ∉ keys(level_to_cell)
-        level_to_cell[level] = Array{Cell}()
+        level_to_cell[level] = Array{Cell, 1}()
     end
-    push!(level_to_cell, cell)
+    push!(level_to_cell[level], cell)
 
     for child in cell.children
         build_info(child, cell_to_level, level_to_cell, level + 1)
     end
 end
 
-function diffuse_intra_cell_proteins(cell_tree::CellTree)
-    CellTreeMod.traverse(cell -> diffuse_intra_cell_proteins_for_cell(cell), cell_tree)
+function diffuse_intra_cell_proteins(tree::CellTree)
+    CellTreeMod.traverse(cell -> diffuse_intra_cell_proteins_for_cell(cell), tree)
 end
 
 function diffuse_intra_cell_proteins_for_cell(cell::Cell)
     cols = length(cell.gene_states)
-    intra_cell_proteins = ProteinStoreMod.get_by_target(cell.proteins, ProteinPropsMod.Intra)
+    intra_cell_proteins = get_all_but_diffusion(cell.proteins)
     
     for protein in intra_cell_proteins
         new_concs = zeros(Float64, cols)
@@ -65,97 +65,111 @@ end
 
 function diffuse_inter_cell_proteins(tree::CellTree)
     #get a set of all of the inter-cell proteins' props (across all cells in the tree)
-    inter_proteins = values(tree.inter_proteins)
+    inter_proteins = get_all_diffusion_proteins(tree)
     info = TreeInfo(tree)
 
     #for each protein, traverse the tree and diffuse it in each cell - storing the new concs in a dictionary
-    results = Dict{ProteinProps, Dict{Cell, Float64}}()
-
     for protein in inter_proteins
+        results = Dict{Tuple{Int64, Int64}, Array{Float64, 1}}()
         CellTreeMod.traverse_bf(cell -> diffuse_inter_cell_protein(cell, protein, results, info), tree)
-    end
-
-    #update the current protein's concs in each cell in the tree
-    if length(results) > 0 #note: it's possible there may not be any inter-cellular proteins...
-        CellTreeMod.traverse(cell -> update_concs(cell, results), tree)
-    end
-end
-
-function diffuse_inter_cell_protein(cell::Cell, protein::InterProtein, results::Dict{ProteinProps, Dict{Cell, Float64}}, info::TreeInfo)
-    level = info.cell_to_level[cell]
-    num_cols = length(info.level_to_cell[level])
-    
-    #if the protein that's diffusing doesn't exist in this cell, create it (with zeroed concs).
-    #Note: If the diffusion results in zeroed or very low concs, the decay step will remove it later.
-    # if protein == nothing
-    #     protein = Protein(cell.config, deepcopy(props), false, false, length(cell.gene_states))
-    #     ProteinStoreMod.insert(cell.proteins, protein, false)
-    # end
-    
-    new_concs = zeros(Float64, num_cols)
-    for j in 1:num_cols
-        new_concs[j] = (1 - 4 * cell.config.run.diff_dt * cell.config.run.diff_alpha / cell.config.run.diff_h^2) * protein.concs[j] +
-            cell.config.run.diff_dt * cell.config.run.diff_alpha * ((get_2D(cell, protein, 0, j - 1, num_cols) +
-                                                       get_2D(cell, protein, -1, j, num_cols) +
-                                                       get_2D(cell, protein, 1, j, num_cols) +
-                                                       get_2D(cell, protein, 0, j + 1, num_cols)) /
-                                                      cell.config.run.diff_h^2)
-        #make sure the value stays in [0.0, 1.0]
-        #note: not sure if this is guarenteed here because of the way we're diffusing between parents and children in the tree structure (it would be in a matrix)...
-        new_concs[j] = clamp(new_concs[j], 0.0, 1.0)
-    end
-
-    #note: we cannot update protein.concs until the whole tree has been traversed (otherwise subsequent
-    #calls may use the updated value instead of the original), since this cell may be referred to more than once.
-    #So we save it in this dictionary instead.
-    if cell ∉ keys(results)
-        results[cell] = Dict{ProteinProps, Array{Float64, 1}}()
-    end
-    results[cell][props] = new_concs
-end
-
-#note: row_offset must be in [-1, 0, 1]
-function get_2D(cell::Cell, protein::InterProtein, row_offset::Int64, col::Int64, width::Int64, info::TreeInfo)
-    outside = col < 1 || col > width
-    outside = outside || (row_offset == -1 && cell.parent == nothing)
-    outside = outside || (row_offset == 1 && length(cell.children) == 0)
-
-    if outside
-        return 0.0
-    else
-        if row_offset == -1
-            num_siblings = length(cell.parent.children)
-            return get_from_cell(cell.parent, protein.props, col) / num_siblings
-            
-        elseif row_offset == 0
-            return protein.concs[col]
-            
-        else
-            sum = 0.0
-            for child in cell.children
-                sum += get_from_cell(child, protein.props, col)
-            end
-
-            return sum
+        #update the concs
+        for ((level, col), new_concs) in results
+            #println(new_concs)
+            CellMod.get_protein(info.level_to_cell[level][col], protein.props).concs = new_concs
         end
     end
 end
 
-function get_from_cell(cell::Cell, props::ProteinProps, col::Int64)
-    protein = ProteinStoreMod.get(cell.proteins, props)
+function diffuse_inter_cell_protein(cell::Cell, protein::InterProtein, results::Dict{Tuple{Int64, Int64}, Array{Float64, 1}}, info::TreeInfo)
+    #if the protein that's diffusing doesn't exist in this cell, create it (with zeroed concs).
+    #Note: If the diffusion results in zeroed or very low concs, the decay step will remove it later.
     if protein == nothing
-        return 0.0
-    else
-        return protein.concs[col]
+        protein = Protein(cell.config, deepcopy(props), false, false, length(cell.gene_states))
+        ProteinStoreMod.insert(cell.proteins, protein, false)
     end
+
+    cell_level = info.cell_to_level[cell]
+    cell_col = findall(n -> n == cell, info.level_to_cell[cell_level])[1]
+
+    new_concs = Array{Float64, 1}()
+    for i in 1:num_concs
+        new_conc = (1 - 4 * diff_dt * diff_alpha / diff_h^2) *
+            cell.concs[i] +
+            diff_dt * diff_alpha * ((
+                get_2D(protein, cell_level, cell_col, i, 0, -1, info, num_concs) +
+                get_2D(protein, cell_level, cell_col, i, -1, 0, info, num_concs) +
+                get_2D(protein, cell_level, cell_col, i, 1, 0, info, num_concs) +
+                get_2D(protein, cell_level, cell_col, i, 0, 1, info, num_concs)
+            ) / diff_h^2)
+        
+        #make sure the value stays in [0.0, 1.0]
+        #note: not sure if this is guarenteed here because of the way we're diffusing between parents and children in the tree structure (it would be in a matrix)...
+        new_concs[j] = clamp(new_concs[j], 0.0, 1.0)
+        push!(new_concs, new_conc)
+    end
+
+    results[(node_level, node_col)] = new_concs
 end
 
-function update_concs(cell::Cell, results::Dict{Cell, Dict{ProteinProps, Array{Float64, 1}}})
-    #note: the protein should always be present here, since we inserted it if it wasn't there in diffuse_inter_cell_proteins()
-    for (props, new_concs) in results[cell]
-        protein = ProteinStoreMod.get(cell.proteins, props)
-        protein.concs = new_concs
+#note: this function will not handle diagonals (i.e. one of {level_offset, col_offset} must be set to 0)
+function get_2D(protein::Protein, cell_level::Int64, cell_col::Int64, conc_col::Int64, row_offset::Int64, col_offset::Int64, info::TreeInfo, num_conc_cols::Int64)
+    cur_cell = info.level_to_cell[cell_level][cell_col]
+    result = 0.0
+
+    #get from above
+    if row_offset == -1 && col_offset == 0
+        if cur_cell.parent != nothing
+            target_conc_col = conc_col + col_offset
+            if target_conc_col != 0 && target_conc_col != num_conc_cols + 1 #if not falling off left or right edge
+                num_siblings = length(cur_cell.parent.children)
+                result = get_protein(protein, cur_cell.parent).concs[target_conc_col] / num_siblings
+            end
+        end
+
+    #get from left
+    elseif row_offset == 0 && col_offset == -1
+        target_conc_col = conc_col + col_offset
+        if target_conc_col == 0 #fall off the left edge
+            #check to see if we have a left sibling we can grab from
+            if cell_col > 1 #have left sibling
+                sibling = info.level_to_cell[cell_level][cell_col - 1]
+                result = get_protein(protein, sibling).concs[num_conc_cols] #return the rightmost conc
+            end
+        else #within bounds in current cell
+            result = get_protein(protein, cur_cell).concs[target_conc_col]
+        end
+
+    #get from right
+    elseif row_offset == 0 && col_offset == 1
+        target_conc_col = conc_col + col_offset
+        if target_conc_col == num_conc_cols + 1 #fall off the right edge
+            #check to see if we have a right sibling we can grab from
+            if cell_col < length(info.level_to_cell[cell_level]) #have right sibling
+                sibling = info.level_to_cell[cell_level][cell_col + 1]
+                result = get_protein(protein, sibling).concs[1] #return the leftmost conc
+            end
+        else #within bounds
+            result = get_protein(protein, cur_cell).concs[target_conc_col]
+        end
+
+    #get from below
+    elseif row_offset == 1 && col_offset == 0
+        if length(cur_cell.children) > 0
+            target_conc_col = conc_col + col_offset
+            if target_conc_col != 0 && target_conc_col != num_conc_cols + 1 #if not falling off left or right edge
+                sum = 0.0
+                for child in cur_cell.children
+                    sum += get_protein(protein, child).concs[target_conc_col]
+                end
+                result = sum
+            end
+        end
+
+    else
+        error("Invalid offsets")
     end
+
+    result
 end
 
 end
--- test/diffusion.jl
+++ test/diffusion.jl
@@ -1,4 +1,5 @@
 using Printf
+using DataStructures
 
 function print_matrix(M)
     rows, cols = size(M)
@@ -69,25 +70,34 @@ function test_1d()
     end
 end
 
-struct Node
-    conc::Float64
+mutable struct Node
+    concs::Array{Float64, 1}
     children::Array{Node, 1}
+    parent::Union{Node, Nothing}
 end
 
-function build_tree()
-    node = Node(0.5, [])
+function build_tree(num_concs::Int64)
+    n0 = Node(repeat([1.0], num_concs), [], nothing)
+    n1 = Node(zeros(num_concs), [], nothing)
+    n2 = Node(zeros(num_concs), [], nothing)
+    n3 = Node(zeros(num_concs), [], nothing)
+    n4 = Node(zeros(num_concs), [], nothing)
+    n5 = Node(zeros(num_concs), [], nothing)
+    n6 = Node(zeros(num_concs), [], nothing)
 
-    nodes = map(copy, repeat(node, 7))
+    push!(n0.children, n1, n2)
+    n1.parent = n0
+    n2.parent = n0
 
-    root = nodes[1]
-    l1l = nodes[2]
-    l1r = nodes[3]
-    push!(root.children, l1l, l1r)
+    push!(n1.children, n3)
+    n3.parent = n1
 
-    push!(l1l.children, nodes[4])
-    push!(l1r.children, nodes[5:7])
+    push!(n2.children, n4, n5, n6)
+    n4.parent = n2
+    n5.parent = n2
+    n6.parent = n2
 
-    root
+    n0
 end
 
 struct TreeInfo
@@ -97,20 +107,20 @@ struct TreeInfo
     function TreeInfo(root::Node)
         node_to_level = Dict{Node, Int64}()
         level_to_node = Dict{Int64, Array{Node, 1}}()
-        if tree.root != nothing
-            build_info(root, node_to_level, level_to_node, 0)
+        if root != nothing
+            build_info(root, node_to_level, level_to_node, 1)
         end
 
-        TreeInfo(node_to_level, level_to_node)
+        new(node_to_level, level_to_node)
     end
 end
 
 function build_info(node::Node, node_to_level::Dict{Node, Int64}, level_to_node::Dict{Int64, Array{Node, 1}}, level::Int64)
     node_to_level[node] = level
     if level ∉ keys(level_to_node)
-        level_to_node[level] = Array{Node}()
+        level_to_node[level] = Array{Node, 1}()
     end
-    push!(level_to_node, node)
+    push!(level_to_node[level], node)
 
     for child in node.children
         build_info(child, node_to_level, level_to_node, level + 1)
@@ -118,56 +128,89 @@ function build_info(node::Node, node_to_level::Dict{Node, Int64}, level_to_node:
 end
 
 #note: this function will not handle diagonals (i.e. one of {level_offset, col_offset} must be set to 0)
-function get_2D(node_level::Int64, node_col::Int64, level_offset::Int64, col_offset::Int64, info::TreeInfo)
-    width = length(info.level_to_node[level])
-    node = info.level_to_node[node_level][node_col]
-    target_col = node_col + node_offset
-    target_level = node_level + level_offset
-    
-    outside = target_col < 1 || target_col > width
-    outside = outside || (level_offset == -1 && node.parent == nothing)
-    outside = outside || (level_offset == 1 && length(node.children) == 0)
+function get_2D(node_level::Int64, node_col::Int64, conc_col::Int64, row_offset::Int64, col_offset::Int64, info::TreeInfo, num_conc_cols::Int64)
+    cur_node = info.level_to_node[node_level][node_col]
+    result = 0.0
 
-    if outside
-        return 0.0
-    else
-        if row_offset == -1 && col_offset == 0
-            num_siblings = length(node.parent.children)
-            return node.parent.conc / num_siblings
-
-        #note: there will be no edge connecting the nodes invoved here (they'll be siblings)
-        elseif row_offset == 0 && col_offset != 0
-            return info.level_to_node[target_level][target_col]
-            
-        elseif row_offset == 1 && col_offset == 0
-            sum = 0.0
-            for child in node.children
-               sum += child.conc
+    #get from above
+    if row_offset == -1 && col_offset == 0
+        if cur_node.parent != nothing
+            target_conc_col = conc_col + col_offset
+            if target_conc_col != 0 && target_conc_col != num_conc_cols + 1 #if not falling off left or right edge
+                num_siblings = length(cur_node.parent.children)
+                result = cur_node.parent.concs[target_conc_col] / num_siblings
             end
-            return sum
         end
+
+    #get from left
+    elseif row_offset == 0 && col_offset == -1
+        target_conc_col = conc_col + col_offset
+        if target_conc_col == 0 #fall off the left edge
+            #check to see if we have a left sibling we can grab from
+            if node_col > 1 #have left sibling
+                sibling = info.level_to_node[node_level][node_col - 1]
+                result = sibling.concs[num_conc_cols] #return the rightmost conc
+            end
+        else #within bounds in current node
+            result = cur_node.concs[target_conc_col]
+        end
+
+    #get from right
+    elseif row_offset == 0 && col_offset == 1
+        target_conc_col = conc_col + col_offset
+        if target_conc_col == num_conc_cols + 1 #fall off the right edge
+            #check to see if we have a right sibling we can grab from
+            if node_col < length(info.level_to_node[node_level]) #have right sibling
+                sibling = info.level_to_node[node_level][node_col + 1]
+                result = sibling.concs[1] #return the leftmost conc
+            end
+        else #within bounds
+            result = cur_node.concs[target_conc_col]
+        end
+
+    #get from below
+    elseif row_offset == 1 && col_offset == 0
+        if length(cur_node.children) > 0
+            target_conc_col = conc_col + col_offset
+            if target_conc_col != 0 && target_conc_col != num_conc_cols + 1 #if not falling off left or right edge
+                sum = 0.0
+                for child in cur_node.children
+                    sum += child.concs[target_conc_col]
+                end
+                result = sum
+            end
+        end
+
+    else
+        error("Invalid offsets")
     end
+
+    result
 end
 
 #results dict is (level, col) -> new_conc
-function diffuse_node(node::Node, info::TreeInfo, results::Dict{Tuple(Int64, Int64), Float64})
+function diffuse_node(node::Node, info::TreeInfo, results::Dict{Tuple{Int64, Int64}, Array{Float64, 1}}, num_concs::Int64)
     diff_alpha = 0.1
     diff_h = 0.75
     diff_dt = 1.0
     
-    level = info.node_to_level[node]
-    col = indexof(node, info.level_to_node[level])[1]
+    node_level = info.node_to_level[node]
+    node_col = findall(n -> n == node, info.level_to_node[node_level])[1]
 
-    new_conc = (1 - 4 * diff_dt * diff_alpha / diff_h^2) *
-        node.conc +
-        diff_dt * diff_alpha * ((
-            get_2D(level, col, 0, -1, info) +
-            get_2D(level, col, -1, 0, info) +
-            get_2D(level, col, 1, 0, info) +
-            get_2D(level, col, 0, 1, info)
-        ) / diff_h^2)
+    new_concs = Array{Float64, 1}()
+    for i in 1:num_concs
+        new_conc = (1 - 4 * diff_dt * diff_alpha / diff_h^2) *
+            node.concs[i] +
+            diff_dt * diff_alpha * ((
+                get_2D(node_level, node_col, i, 0, -1, info, num_concs) +
+                get_2D(node_level, node_col, i, -1, 0, info, num_concs) +
+                get_2D(node_level, node_col, i, 1, 0, info, num_concs) +
+                get_2D(node_level, node_col, i, 0, 1, info, num_concs)
+            ) / diff_h^2)
+        push!(new_concs, new_conc)
+    end
 
-    results[(level, col)] = new_conc
+    results[(node_level, node_col)] = new_concs
 end
 
 #breadth-first traversal
@@ -177,7 +220,7 @@ function traverse_bf(f::Function, start_node::Node)
 
     while !isempty(q)
         node = dequeue!(q)
-        f(cell)
+        f(node)
         
         for child in node.children
             enqueue!(q, child)
@@ -185,37 +228,40 @@ function traverse_bf(f::Function, start_node::Node)
     end
 end
 
-function diffuse_tree(root::Node)
+function diffuse_tree(root::Node, num_concs::Int64)
     info = TreeInfo(root)
-    results = Dict{Tuple(Int64, Int64), Float64}()
+    results = Dict{Tuple{Int64, Int64}, Array{Float64, 1}}()
 
     #apply the diffusion
-    traverse_bf(node -> diffuse_node(node, info, results), root)
+    traverse_bf(node -> diffuse_node(node, info, results, num_concs), root)
 
     #update the concs
-    for ((level, col), new_conc) in results
-        info.level_to_node[level][col].conc = new_conc
+    for ((level, col), new_concs) in results
+        #println(new_concs)
+        info.level_to_node[level][col].concs = new_concs
     end
 end
 
 function print_tree(root::Node)
     info = TreeInfo(root)
-    for (level, nodes) in info.level_to_node
-        for node in nodes
-            @printf("%5.2f", node.conc)
+    for level in 1:length(info.level_to_node)
+        for node in info.level_to_node[level]
+            @printf("%s, ", node.concs)
         end
         print("\n")
     end
+    print("\n")
 end
 
 function test_tree()
-    root = build_tree()
-    print_tree(root)
-
     steps = 3
+    num_concs = 5
+    
+    root = build_tree(num_concs)
+    print_tree(root)
     
     for i in 1:steps
-        diffuse_tree(root)
+        diffuse_tree(root, num_concs)
         print_tree(root)
     end
 end
